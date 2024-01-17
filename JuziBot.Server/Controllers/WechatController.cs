01a275b2327e21a77eede6f4df49ae6fd9d25e75
Comiscience <hu-beau@outlook.com>
2024-01-17 18:54:10 +0800

    first upload

---
+++ JuziBot.Server/Controllers/WechatController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using System.Net;
+using System.Text.Json;
+
+namespace JuziBot.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WechatController : ControllerBase
+    {
+        [HttpPost]
+        [Route("ReceiveMessageCallback")]
+        public void ReceiveMessageCallback([FromBody] dynamic body)
+        {
+            Console.WriteLine(body.ToString());
+        }
+    }
+
+}
