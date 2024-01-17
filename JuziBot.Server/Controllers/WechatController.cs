8c51d1aeef315521b0ef3f14178449fc5615d193
Comiscience <hu-beau@outlook.com>
2024-01-17 21:48:46 +0800

    working in progress

--- JuziBot.Server/Controllers/WechatController.cs
+++ JuziBot.Server/Controllers/WechatController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JuziBot.Server.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Dynamic;
 using System.Net;
@@ -19,11 +20,16 @@ namespace JuziBot.Server.Controllers
 
         [HttpPost]
         [Route("ReceiveMessageCallback")]
-        public void ReceiveMessageCallback([FromBody] dynamic body)
+        public async Task ReceiveMessageCallback([FromBody]WechatMessageModel body)
         {
-            string bodyStr = body.ToString();
-            Console.WriteLine(bodyStr);
+            string bodyStr = JsonSerializer.Serialize(body);
             _logger.LogInformation(bodyStr);
+            if(!string.IsNullOrWhiteSpace(body.payload.text) && 
+                body.payload.text.Trim().StartsWith("http://mp.weixin.qq.com/"))
+            {
+                var url = body.payload.text.Trim().Replace("\\u0026", "&").Replace(" ", "");
+                string htmlContent = await GetArticleContentAsync(url);
+            }
         }
 
 
@@ -31,18 +37,22 @@ namespace JuziBot.Server.Controllers
         [Route("RetrieveArticleContent")]
         public async Task RetrieveArticleContent()
         {
-            using var client = new HttpClient();
-            client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
-
-            var content = await client.GetStringAsync("http://mp.weixin.qq.com/s?__biz=MjM5MzE3NzE1OA==&mid=2247508851&idx=1&sn=906ff32495cafe12bdc59cc0ee0ee576&chksm=a699e85a91ee614cb5e793bd39f434e26bb1f13b5660d26162815dbaa0d3aae42e7e515ed8cc&mpshare=1&scene=1&srcid=0114zTvWnKU68Z7OiOwzykP9&sharer_shareinfo=ef4e7acd1e73a0e0987db471f8e8749b&sharer_shareinfo_first=81d2d798cd2d65f3253f4f97c5cd9047#rd");
-
-
-            var startIndex = content.IndexOf("<blockquote class=\"js_blockquote_wrap\"");
-            var startStr = content.Substring(startIndex);
-            var endIndex = startStr.IndexOf("</blockquote");
-            var finalStr = startStr.Substring(0, endIndex);
+            string finalStr = await GetArticleContentAsync("http://mp.weixin.qq.com/s?__biz=MjM5MzE3NzE1OA==&mid=2247508851&idx=1&sn=906ff32495cafe12bdc59cc0ee0ee576&chksm=a699e85a91ee614cb5e793bd39f434e26bb1f13b5660d26162815dbaa0d3aae42e7e515ed8cc&mpshare=1&scene=1&srcid=0114zTvWnKU68Z7OiOwzykP9&sharer_shareinfo=ef4e7acd1e73a0e0987db471f8e8749b&sharer_shareinfo_first=81d2d798cd2d65f3253f4f97c5cd9047#rd");
             Console.WriteLine(finalStr);
             _logger.LogInformation(finalStr);
+        }
+
+        private async Task<string> GetArticleContentAsync(string url)
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Yo Young Fella");
+
+            var content = await client.GetStringAsync(url);
+            var startIndex = content.IndexOf("id=\"js_content\"");
+            var startStr = content.Substring(startIndex);
+            var endIndex = startStr.IndexOf("<script type=\"text/javascript\"");
+            var finalStr = startStr.Substring(0, endIndex);
+            return finalStr;
         }
 
     }
---
+++ JuziBot.Server/Models/WechatMessageModel.cs
@@ -0,0 +1,31 @@
+﻿namespace JuziBot.Server.ViewModels
+{
+    public class WechatMessageModel
+    {
+        public string orgId { get; set; }
+        public string token { get; set; }
+        public string botId { get; set; }
+        public string imBotId { get; set; }
+        public string botUserId { get; set; }
+        public string chatId { get; set; }
+        public string avatar { get; set; }
+        public bool coworker { get; set; }
+        public string imContactId { get; set; }
+        public string externalUserId { get; set; }
+        public string contactName { get; set; }
+        public int contactType { get; set; }
+        public string messageId { get; set; }
+        public bool isSelf { get; set; }
+        public string sendBy { get; set; }
+        public int source { get; set; }
+        public long timestamp { get; set; }
+        public int messageType { get; set; }
+        public WechatMessagePayload payload { get; set; }
+    }
+
+    public class WechatMessagePayload
+    {
+        public List<object> mention { get; set; }
+        public string text { get; set; }
+    }
+}
